44d28f1833cd317b123287f19631e2e8b1c9a19f
Nafis Ahamed <nafisahamed14@gmail.com>
2024-03-12 05:18:48 +0600

    third commit

--- second-project/src/App.jsx
+++ second-project/src/App.jsx
@@ -1,15 +1,35 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Watch from './components/sunglass/Watch/Watch'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [watchs, setWatches] = useState([])
+
+  useEffect(()=>{
+     fetch('watch.json')
+     .then(res => res.json())
+     .then(data => setWatches(data))
+
+  },[])
+   
+  // const watchs = [
+  //   { id: 1, name: 'Rolex Submariner', price: 10000 },
+  //   { id: 2, name: 'Omega Speedmaster Professional', price: 5000 },
+  //   { id: 3, name: 'Seiko Presage', price: 800 },
+  //   { id: 4, name: 'Casio G-Shock', price: 100 },
+  //   { id: 5, name: 'Tag Heuer Carrera', price: 3000 },
+  // ];
 
   return (
     <>
         
       <h1>Vite + React</h1>
+      {
+        watchs.map(watch => <Watch key={watch.id} watch = {watch}></Watch> )
+      }
     
     </>
   )
--- second-project/src/components/sunglass/Watch/Watch.jsx
+++ second-project/src/components/sunglass/Watch/Watch.jsx
@@ -1,10 +1,12 @@
 
 import React from 'react';
 
-const Watch = () => {
+const Watch = ({watch}) => {
+    const {name,price} = watch
     return (
         <div>
-            
+            <h2>Watchs:{name}</h2>
+            <p>{price}</p>
         </div>
     );
 };
